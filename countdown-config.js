40f811c9ee20d495aa0350f491b7d99ce69c863e
gillosae <hyway@snu.ac.kr>
2026-03-13 14:59:37 +0900

    change date

--- countdown-config.js
+++ countdown-config.js
@@ -8,28 +8,28 @@
 const config = {
     events: [
         {
-            name: "EMNLP ARR",
-            date: "2026-03-17",
-            time: "21:00:00"
-        },
-        {
             name: "MM",
             date: "2026-04-02",
             time: "21:00:00"
         },
         {
             name: "ASSETS",
-            date: "2026-04-22",
+            date: "2026-04-23",
             time: "23:59:59"
         },
         {
             name: "ISMIR",
-            date: "2026-04-20",
+            date: "2026-04-28",
             time: "21:00:00"
         },
         {
             name: "NeurIPS",
-            date: "2026-05-07",
+            date: "2026-05-08",
+            time: "21:00:00"
+        },
+        {
+            name: "EMNLP ARR",
+            date: "2026-05-26",
             time: "21:00:00"
         }
     ]
