647f0a8f9bb7eb1d1b7a8c1148724232bae44d10
gillosae <hyway@snu.ac.kr>
2026-05-30 06:24:04 +0900

    DDD

--- countdown-config.js
+++ countdown-config.js
@@ -14,14 +14,14 @@ const config = {
             est: false
         },
         {
-            name: "ICASSP",
-            date: "2026-09-17",
+            name: "CHI",
+            date: "2026-09-11",
             time: "21:00:00",
             est: false
         },
         {
-            name: "CHI",
-            date: "2026-09-11",
+            name: "ICASSP",
+            date: "2026-09-17",
             time: "21:00:00",
             est: false
         },
@@ -32,7 +32,7 @@ const config = {
             est: true
         },
         {
-            name: "AAAI",
+            name: "CVPR",
             date: "2026-11-14",
             time: "21:00:00",
             est: true
